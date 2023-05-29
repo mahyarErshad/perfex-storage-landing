c1a28888d47bc5eaf174f2d0effeec139da54b28
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 09:13:34 +0330

    Imported HeroSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import HeroSection from "./Components/HeroSection/HeroSection";
 
 function App() {
   return (
-    <h1 className="text-white text-8xl">
-      سلام
-    </h1>
+    <>
+      <HeroSection />
+    </>
   );
 }
 
