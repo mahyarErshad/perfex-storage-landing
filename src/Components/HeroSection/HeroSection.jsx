087520ac0c1c4826397f00d6de68ae2c0aa0fc84
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 12:43:14 +0330

    Added image to the dom

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CustomButton from "../Utils/CustomButton/CustomButton";
-import image from "../../assets/images/hero-section-image.png"
+import image from "../../assets/images/hero-section-image.png";
 
 function HeroSection() {
   return (
@@ -8,6 +8,7 @@ function HeroSection() {
       <h1 className="font-bold text-[3.5rem] gradient-text">ماژول انبارداری اسکریپت پرفکس</h1>
       <p className="mt-8 text-center text-base text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
       <CustomButton className="mt-11" />
+      <img src={image} alt="محیط کاربری ماژول انبارداری اسکریپت پرفکس" />
     </section>
   );
 }
