298dc7564904630ef3fd1c465fd02b3b90e0352c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 09:18:53 +0330

    H1 is added and styled

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function HeroSection() {
   return (
-    <section className='container'>HeroSection</section>
-  )
+    <section className="container flex-center pt-[6.5rem]">
+      <h1 className="font-bold text-[3.5rem] gradient-text">ماژول انبارداری اسکریپت پرفکس</h1>
+    </section>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
