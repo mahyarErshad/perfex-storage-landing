ead7384e37072c2683ee5bbca1fef9ea72b687aa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 13:02:54 +0330

    Added another shape span

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -8,11 +8,12 @@ import { ReactComponent as Arrow } from "../../assets/images/twisted-arrow.svg";
 function HeroSection() {
   return (
     <section className="container flex-center pt-[6.5rem] relative">
-    <span className="w-[11rem] h-[11rem] bg-primary absolute top-0 right-0 max-lg:hidden translate-x-[150%] rounded-full blur-[10.3125rem]"></span>
+      <span className="w-[11rem] h-[11rem] bg-primary absolute top-0 right-0 max-lg:hidden translate-x-[150%] blur-[10.3125rem]"></span>
       <h1 className="font-bold max-lg:text-3xl text-[3.5rem] gradient-text text-center">ماژول انبارداری اسکریپت پرفکس</h1>
       <p className="mt-8 text-center text-base text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
       <CustomButton className="mt-11" />
       <div className="relative">
+        <span className="w-[11rem] h-[11rem] bg-primary absolute top-0 right-0 max-lg:hidden translate-x-[150%] blur-[8.5625rem]"></span>
         <img src={image} alt="محیط کاربری ماژول انبارداری اسکریپت پرفکس" />
         <img src={topRightBoxes} className="absolute top-0 right-0 translate-x-[50%] -translate-y-[50%] max-lg:hidden" alt="Boxes" />
         <img src={bottomLeftBoxes} className="absolute bottom-0 left-0 -translate-x-[50%] translate-y-[45%] max-lg:hidden" alt="Boxes" />
