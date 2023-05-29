60ccf15b5360c27b29f544b77d1572fc1ff5dc43
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 10:37:35 +0330

    Position is relative

--- src/Components/Utils/CustomButton/CustomButton.jsx
+++ src/Components/Utils/CustomButton/CustomButton.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 
 function CustomButton({ className, text, href }) {
   return (
-    <a href={href} target="blank" className={`${className} ${styles.button} py-4 min-w-[12.5rem] flex-center text-white text-xl font-bold cursor-pointer duration-300`}>
+    <a href={href} target="blank" className={`${className} ${styles.button} py-4 min-w-[12.5rem] flex-center text-white text-xl font-bold cursor-pointer duration-300 relative`}>
       {text || "خرید ماژول"}
     </a>
   );
