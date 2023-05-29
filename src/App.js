f1a5f70ff753573da774e104813ee717bd673bb9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 09:05:27 +0330

    Font is Kalameh

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="text-red-500">
-      hello world
-    </div>
+    <h1 className="text-red-500 text-8xl">
+      سلام
+    </h1>
   );
 }
 
