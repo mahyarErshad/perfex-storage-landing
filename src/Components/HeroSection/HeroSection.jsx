1cb48c559e5829692035789063e5929b3bf889ca
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 12:54:57 +0330

    On lg screens the shapes are hidden

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -7,15 +7,15 @@ import { ReactComponent as Arrow } from "../../assets/images/twisted-arrow.svg";
 
 function HeroSection() {
   return (
-    <section className="container flex-center pt-[6.5rem]">
+    <section className="container flex-center pt-[6.5rem] relative">
       <h1 className="font-bold text-[3.5rem] gradient-text">ماژول انبارداری اسکریپت پرفکس</h1>
       <p className="mt-8 text-center text-base text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
       <CustomButton className="mt-11" />
       <div className="relative">
         <img src={image} alt="محیط کاربری ماژول انبارداری اسکریپت پرفکس" />
-        <img src={topRightBoxes} className="absolute top-0 right-0 translate-x-[50%] -translate-y-[50%]" alt="Boxes" />
-        <img src={bottomLeftBoxes} className="absolute bottom-0 left-0 -translate-x-[50%] translate-y-[45%]" alt="Boxes" />
-        <Arrow className="absolute top-0 left-0 -translate-x-[80%] -translate-y-[80%]" />
+        <img src={topRightBoxes} className="absolute top-0 right-0 translate-x-[50%] -translate-y-[50%] max-lg:hidden" alt="Boxes" />
+        <img src={bottomLeftBoxes} className="absolute bottom-0 left-0 -translate-x-[50%] translate-y-[45%] max-lg:hidden" alt="Boxes" />
+        <Arrow className="absolute top-0 left-0 -translate-x-[80%] -translate-y-[80%] max-lg:hidden" />
       </div>
     </section>
   );
