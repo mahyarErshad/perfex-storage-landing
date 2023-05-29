3a6d356afbaf72818c4e7ef774c20bdbaf5721c7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 11:03:14 +0330

    Removed gradient from the button

--- src/Components/Utils/CustomButton/CustomButton.jsx
+++ src/Components/Utils/CustomButton/CustomButton.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as BackgroundShape } from "../../../assets/images/button
 function CustomButton({ className, text, href }) {
   return (
     <>
-      <a href={href} target="blank" className={`${className} ${styles.button} py-4 min-w-[12.5rem] flex-center text-white text-xl font-bold cursor-pointer duration-300 z-10`}>
+      <a href={href} target="blank" className={`${className} ${styles.button} py-4 bg-primary min-w-[12.5rem] flex-center text-white text-xl font-bold cursor-pointer duration-300 z-10`}>
         {text || "خرید ماژول"}
       </a>
       <BackgroundShape className="-translate-y-[85%] z-0" />
