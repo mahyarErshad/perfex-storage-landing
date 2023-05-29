2d13c5bbfd321bd7019a7b61d2c6e390d054fffe
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 12:51:26 +0330

    bottomLeftBoxes is placed

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -14,6 +14,7 @@ function HeroSection() {
       <div className="relative">
         <img src={image} alt="محیط کاربری ماژول انبارداری اسکریپت پرفکس" />
         <img src={topRightBoxes} className="absolute top-0 right-0 translate-x-[50%] -translate-y-[50%]" alt="Boxes" />
+        <img src={bottomLeftBoxes} className="absolute bottom-0 left-0 -translate-x-[50%] translate-y-[45%]" alt="Boxes" />
       </div>
     </section>
   );
