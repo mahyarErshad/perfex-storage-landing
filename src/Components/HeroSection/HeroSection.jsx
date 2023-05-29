628918f0faaa2a5dd0a8daf582528462a1512371
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 10:29:32 +0330

    CustomButton is fully designed

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ function HeroSection() {
     <section className="container flex-center pt-[6.5rem]">
       <h1 className="font-bold text-[3.5rem] gradient-text">ماژول انبارداری اسکریپت پرفکس</h1>
       <p className="mt-8 text-center text-base text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
-      <CustomButton text="خرید ماژول" />
+      <CustomButton className="mt-11" />
     </section>
   );
 }
--- src/Components/Utils/CustomButton/CustomButton.jsx
+++ src/Components/Utils/CustomButton/CustomButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-function CustomButton(className, text, href) {
+function CustomButton({ className, text, href }) {
   return (
-    <a href={href} target="blank" className={`${className} ${styles.button} py-4 min-w-[12.5rem]`}>
-      {text}
+    <a href={href} target="blank" className={`${className} ${styles.button} py-4 min-w-[12.5rem] flex-center text-white text-xl font-bold cursor-pointer duration-300`}>
+      {text || "خرید ماژول"}
     </a>
   );
 }
