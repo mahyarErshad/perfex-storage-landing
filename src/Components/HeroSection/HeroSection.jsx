a8ee637dc784cfb27600ec269594e2d5c4189372
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-29 12:47:08 +0330

    Wrapped image into a div

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -8,7 +8,9 @@ function HeroSection() {
       <h1 className="font-bold text-[3.5rem] gradient-text">ماژول انبارداری اسکریپت پرفکس</h1>
       <p className="mt-8 text-center text-base text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
       <CustomButton className="mt-11" />
-      <img src={image} alt="محیط کاربری ماژول انبارداری اسکریپت پرفکس" />
+      <div className="relative">
+        <img src={image} alt="محیط کاربری ماژول انبارداری اسکریپت پرفکس" />
+      </div>
     </section>
   );
 }
