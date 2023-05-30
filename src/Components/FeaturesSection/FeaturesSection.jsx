19849bc55168b29f550c7042b005d2a65f6266b1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:56:10 +0330

    Added p tag and the divider

--- src/Components/FeaturesSection/FeaturesSection.jsx
+++ src/Components/FeaturesSection/FeaturesSection.jsx
@@ -8,6 +8,8 @@ function FeaturesSection() {
       <div className="grid lg:grid-cols-4 md:grid-cols-2 max-md:grid-cols-1 gap-6 w-full">
         <div className="flex-col py-10 px-4 glass-container rounded-20 w-full">
           <h3 className="gradient-text text-center font-bold max-lg:text-xl lg:text-2xl">عنوان ویژگی</h3>
+          <span className="h-[0.125rem] w-full bg-gradient mt-6"></span>
+          <p className="mt-8 w-full text-white text-xs leading-6">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
         </div>
       </div>
     </section>
