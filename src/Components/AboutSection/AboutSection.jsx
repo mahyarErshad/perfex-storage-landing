20e9f2e5621c31ef71d886c5cca45786384cf312
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 08:58:16 +0330

    Copied the span here

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AboutSection() {
   return (
     <section className='container mt-[6.5rem] flex-center'>
-    <span className='w-10 h-2 rounded-[1.25rem] '></span>
+
     </section>
   )
 }
--- src/Components/Utils/SectionHeading/SectionHeading.jsx
+++ src/Components/Utils/SectionHeading/SectionHeading.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function SectionHeading() {
   return (
-    <div>SectionHeading</div>
-  )
+    <div className="flex-center flex-col gap-4">
+      <span className="w-10 h-2 rounded-[1.25rem] "></span>
+    </div>
+  );
 }
 
-export default SectionHeading
+export default SectionHeading;
