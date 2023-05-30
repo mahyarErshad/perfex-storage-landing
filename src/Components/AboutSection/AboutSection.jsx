43e823e29fd6374d97fd83810af61345745e3193
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:04:43 +0330

    AboutSection is done

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -4,7 +4,7 @@ import CustomButton from "../Utils/CustomButton/CustomButton";
 
 function AboutSection() {
   return (
-    <section className="container mt-[6.5rem] flex-center">
+    <section className="container mt-[6.5rem] mb-32 flex-center">
       <SectionHeading title="درباره ماژول انبارداری" />
       <div className="mt-8 glass-container pt-10 w-full flex-col flex-center rounded-20 border border-[#ffffff29]">
         <p className="max-w-[68.5rem] text-base text-white text-center leading-10 mb-20">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
