e89076c800e253a241f62d3b1121a020485d7d21
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 09:02:41 +0330

    Added h2 for title

--- src/Components/Utils/SectionHeading/SectionHeading.jsx
+++ src/Components/Utils/SectionHeading/SectionHeading.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-function SectionHeading() {
+function SectionHeading({title}) {
   return (
     <div className="flex-center flex-col gap-4">
       <span className={`w-10 h-2 rounded-[1.25rem] ${styles.gradient}`}></span>
+      <h2>{title}</h2>
     </div>
   );
 }
