e6d7a522020354830619097564bfa69a4e6bd575
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 09:00:41 +0330

    Added gradient style to the span

--- src/Components/Utils/SectionHeading/SectionHeading.jsx
+++ src/Components/Utils/SectionHeading/SectionHeading.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
 function SectionHeading() {
   return (
     <div className="flex-center flex-col gap-4">
-      <span className="w-10 h-2 rounded-[1.25rem] "></span>
+      <span className={`w-10 h-2 rounded-[1.25rem] ${styles.gradient}`}></span>
     </div>
   );
 }
