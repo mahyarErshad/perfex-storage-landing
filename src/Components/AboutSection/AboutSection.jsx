4c5abc658db081db940455eef6c57b7a0fac81c9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 09:01:06 +0330

    Imported SectionHeading

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import SectionHeading from "../Utils/SectionHeading/SectionHeading";
 
 function AboutSection() {
   return (
-    <section className='container mt-[6.5rem] flex-center'>
-
+    <section className="container mt-[6.5rem] flex-center">
+      <SectionHeading />
     </section>
-  )
+  );
 }
 
-export default AboutSection
+export default AboutSection;
