de7cc559cba650a9bd50a77d2f0d7e4b253e175d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 09:09:40 +0330

    Added a div

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -5,6 +5,9 @@ function AboutSection() {
   return (
     <section className="container mt-[6.5rem] flex-center">
       <SectionHeading title="درباره ماژول انبارداری" />
+      <div className="mt-8 glass-container">
+
+      </div>
     </section>
   );
 }
