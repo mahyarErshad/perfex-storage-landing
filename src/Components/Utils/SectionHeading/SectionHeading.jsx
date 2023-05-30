2e8815e6a8598df90b28b1964c01dcc1d0dbe671
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 09:06:04 +0330

    H2 styled and animated

--- src/Components/Utils/SectionHeading/SectionHeading.jsx
+++ src/Components/Utils/SectionHeading/SectionHeading.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-function SectionHeading({title}) {
+function SectionHeading({ title }) {
   return (
-    <div className="flex-center flex-col gap-4">
+    <div className="flex-center flex-col gap-3">
       <span className={`w-10 h-2 rounded-[1.25rem] ${styles.gradient}`}></span>
-      <h2>{title}</h2>
+      <h2 className="max-lg:text-2xl lg:text-[2rem] font-bold text-center gradient-text gradient-text__animation">{title}</h2>
     </div>
   );
 }
