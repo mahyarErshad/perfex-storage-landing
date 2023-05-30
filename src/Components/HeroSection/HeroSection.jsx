9e48a9e872002ebf1198568a8df96c66d15f531b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:49:03 +0330

    Removed gradient text class from elements which have gradient animation

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -10,7 +10,7 @@ function HeroSection() {
     <section className="container flex-center pt-[6.5rem] relative">
       <span className="w-[11rem] h-[11rem] bg-primary absolute top-0 right-0 max-lg:hidden translate-x-[150%] blur-[10.3125rem]"></span>
       <span className="w-[11rem] h-[11rem] bg-primary absolute top-0 left-0 max-lg:hidden -translate-x-[150%] blur-[10.3125rem]"></span>
-      <h1 className="font-bold max-lg:text-3xl text-[3.5rem] gradient-text gradient-text__animation text-center">ماژول انبارداری اسکریپت پرفکس</h1>
+      <h1 className="font-bold max-lg:text-3xl text-[3.5rem] gradient-text__animation text-center">ماژول انبارداری اسکریپت پرفکس</h1>
       <p className="mt-8 text-center text-base text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.</p>
       <CustomButton className="mt-11" />
       <div className="relative">
--- src/Components/Utils/SectionHeading/SectionHeading.jsx
+++ src/Components/Utils/SectionHeading/SectionHeading.jsx
@@ -5,7 +5,7 @@ function SectionHeading({ title }) {
   return (
     <div className="flex-center flex-col gap-3">
       <span className={`w-10 h-2 rounded-20 ${styles.gradient}`}></span>
-      <h2 className="max-lg:text-2xl lg:text-[2rem] font-bold text-center gradient-text gradient-text__animation">{title}</h2>
+      <h2 className="max-lg:text-2xl lg:text-[2rem] font-bold text-center gradient-text__animation">{title}</h2>
     </div>
   );
 }
