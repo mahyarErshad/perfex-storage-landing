a6c2a163663399f18f1af515dd1f915b06b042aa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:19:30 +0330

    Added h2

--- src/Components/FeaturesSection/FeaturesSection.jsx
+++ src/Components/FeaturesSection/FeaturesSection.jsx
@@ -3,8 +3,13 @@ import SectionHeading from "../Utils/SectionHeading/SectionHeading";
 
 function FeaturesSection() {
   return (
-    <section className="container flex-center">
+    <section className="container flex-center mb-[9.5rem] gap-10">
       <SectionHeading title="ویژگی های ماژول انبارداری" />
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 max-md:grid-cols-1 gap-6">
+      <div className="flex-col py-10 px-4 glass-container rounded-20">
+      <h3 className="gradient-text text-center font-bold text-2xl">عنوان ویژگی</h3>
+      </div>
+      </div>
     </section>
   );
 }
