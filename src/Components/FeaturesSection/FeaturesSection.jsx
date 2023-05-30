32579b32a98e6ca5cae4647e9c673dca660ad28c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:44:26 +0330

    Everything is working and fixed the bugs

--- src/Components/FeaturesSection/FeaturesSection.jsx
+++ src/Components/FeaturesSection/FeaturesSection.jsx
@@ -5,8 +5,8 @@ function FeaturesSection() {
   return (
     <section className="container flex-center mb-[9.5rem] gap-10">
       <SectionHeading title="ویژگی های ماژول انبارداری" />
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 max-md:grid-cols-1 gap-6">
-        <div className="flex-col py-10 px-4 glass-container rounded-20">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 max-md:grid-cols-1 gap-6 w-full">
+        <div className="flex-col py-10 px-4 glass-container rounded-20 w-full">
           <h3 className="gradient-text text-center font-bold max-lg:text-xl lg:text-2xl">عنوان ویژگی</h3>
         </div>
       </div>
