cc447a0d14ac14b049e77108f971afb80dedbc6a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:21:41 +0330

    Defined the border style inside the glass-container class

--- src/Components/FeaturesSection/FeaturesSection.jsx
+++ src/Components/FeaturesSection/FeaturesSection.jsx
@@ -6,9 +6,9 @@ function FeaturesSection() {
     <section className="container flex-center mb-[9.5rem] gap-10">
       <SectionHeading title="ویژگی های ماژول انبارداری" />
       <div className="grid lg:grid-cols-4 md:grid-cols-2 max-md:grid-cols-1 gap-6">
-      <div className="flex-col py-10 px-4 glass-container rounded-20">
-      <h3 className="gradient-text text-center font-bold text-2xl">عنوان ویژگی</h3>
-      </div>
+        <div className="flex-col py-10 px-4 glass-container rounded-20">
+          <h3 className="gradient-text text-center font-bold max-lg:text-xl lg:text-2xl">عنوان ویژگی</h3>
+        </div>
       </div>
     </section>
   );
