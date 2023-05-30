0139d792000d3d0d88aa92ec9a1e9a82d5d6f9a3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:58:04 +0330

    Imported FeatureSectionCard

--- src/Components/FeaturesSection/FeaturesSection.jsx
+++ src/Components/FeaturesSection/FeaturesSection.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import SectionHeading from "../Utils/SectionHeading/SectionHeading";
+import FeatureSectionCard from "../Utils/FeatureSectionCard/FeatureSectionCard";
 
 function FeaturesSection() {
   return (
     <section className="container flex-center mb-[9.5rem] gap-10">
       <SectionHeading title="ویژگی های ماژول انبارداری" />
       <div className="grid lg:grid-cols-4 md:grid-cols-2 max-md:grid-cols-1 gap-6 w-full">
-
+        <FeatureSectionCard />
+        <FeatureSectionCard />
+        <FeatureSectionCard />
+        <FeatureSectionCard />
       </div>
     </section>
   );
