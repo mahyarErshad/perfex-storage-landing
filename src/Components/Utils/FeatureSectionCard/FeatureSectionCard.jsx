a56d2d680f7bc55579522428c35469e3f6ad29a3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 10:57:41 +0330

    Copied the codes here

--- src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
+++ src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function FeatureSectionCard() {
   return (
-    <div>FeatureSectionCard</div>
-  )
+    <div className="flex-col py-10 px-4 glass-container rounded-20 w-full">
+      <h3 className="gradient-text text-center font-bold max-lg:text-xl lg:text-2xl">عنوان ویژگی</h3>
+      <span className="h-[0.125rem] w-full bg-gradient mt-6"></span>
+      <p className="mt-8 w-full text-white text-xs leading-6">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+    </div>
+  );
 }
 
-export default FeatureSectionCard
+export default FeatureSectionCard;
