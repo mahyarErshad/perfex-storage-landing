e76b146e9268aa7efc84f37c236af3731780bde1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 11:04:33 +0330

    Styled the after shape

--- src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
+++ src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function FeatureSectionCard() {
+function FeatureSectionCard({ gradientFrom, gradientTo }) {
   return (
-    <div className="flex-col py-10 px-4 glass-container rounded-20 w-full">
+    <div className="flex-col py-10 px-4 glass-container rounded-20 w-full relative">
+      <span className={`absolute -top-1 w-[80%] right-[10%] h-1 rounded-[0.3125rem] bg-gradient-to-r ${gradientFrom} ${gradientTo}`}></span>
       <h3 className="gradient-text text-center font-bold max-lg:text-xl lg:text-2xl">عنوان ویژگی</h3>
       <span className="h-[0.125rem] w-full bg-gradient mt-6"></span>
       <p className="mt-8 w-full text-white text-xs leading-6">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
