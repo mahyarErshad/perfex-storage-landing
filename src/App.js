f2a351939d40892e92e5e09fa8a67bbd3a5c3277
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 09:01:28 +0330

    Imported AboutSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./Components/HeroSection/HeroSection";
+import AboutSection from "./Components/AboutSection/AboutSection";
 
 function App() {
   return (
     <>
       <HeroSection />
+      <AboutSection />
     </>
   );
 }
