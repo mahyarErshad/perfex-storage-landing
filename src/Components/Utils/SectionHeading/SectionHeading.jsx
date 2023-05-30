caad4583fe2b182c55271f25d202bb8aaf7d2903
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-30 09:28:50 +0330

    Defined a class for border radius 20px

--- src/Components/Utils/SectionHeading/SectionHeading.jsx
+++ src/Components/Utils/SectionHeading/SectionHeading.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css";
 function SectionHeading({ title }) {
   return (
     <div className="flex-center flex-col gap-3">
-      <span className={`w-10 h-2 rounded-[1.25rem] ${styles.gradient}`}></span>
+      <span className={`w-10 h-2 rounded-20 ${styles.gradient}`}></span>
       <h2 className="max-lg:text-2xl lg:text-[2rem] font-bold text-center gradient-text gradient-text__animation">{title}</h2>
     </div>
   );
