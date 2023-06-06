b64725f1faba1e5b2f0f7a9e0814e38a0fbfbe96
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 11:28:38 +0330

    Added box shadow for tab

--- src/Components/TabsSection/TabsSection.jsx
+++ src/Components/TabsSection/TabsSection.jsx
@@ -9,9 +9,9 @@ function TabsSection() {
       <SectionHeading title="تصاویر ماژول انبارداری" />
       <TabList className="mt-10 flex-center gap-6">
         {array.map((_, index) => (
-          <Tab className=" flex-center flex-col cursor-pointer duration-300" key={index}>
-            <div className="glass-container rounded-20 py-6 max-h-[4.5rem] text-white w-[12.5rem] font-bold text-center">عنوان تصویر</div>
-            <span className="w-[85%] h-1 bg-gradient-to-r from-[#8BC6EC] to-[#9599E2] rounded-20 duration-300 opacity-0"></span>
+          <Tab className=" flex-center flex-col cursor-pointer duration-300 tab" key={index}>
+            <div className="glass-container rounded-20 py-6 max-h-[4.5rem] text-white w-[12.5rem] font-bold text-center z-10">عنوان تصویر</div>
+            <span className="w-[85%] h-2 -translate-y-1 bg-gradient-to-r from-[#8BC6EC] to-[#9599E2] rounded-20 duration-300 opacity-0 z-0"></span>
           </Tab>
         ))}
       </TabList>
