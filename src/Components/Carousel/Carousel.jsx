d34761a919e3382d9922287b9b80c7c7c7534d0c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 13:29:27 +0330

    Changed some settings of Swiper

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -3,25 +3,26 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const Carousel = () => {
-    const arr = ["دقت بالا در کنترل موجودی در هنگام نقل و انتقال کالاها در بین انبارها", "دقت بالا در کنترل موجودی در هنگام مرجوع شدن کالاها", "مدیریت راحت انبارها", "سرعت در انجام کارهای مرتبط با انبارداری"]
+  const arr = ["دقت بالا در کنترل موجودی در هنگام نقل و انتقال کالاها در بین انبارها", "دقت بالا در کنترل موجودی در هنگام مرجوع شدن کالاها", "مدیریت راحت انبارها", "سرعت در انجام کارهای مرتبط با انبارداری"];
   return (
     <section className="w-full flex-center">
       <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
+        spaceBetween={24}
+        slidesPerView="auto"
         loop={true}
         autoplay={{
-          delay: 3000,
+          delay: 2000,
           disableOnInteraction: false,
           pauseOnMouseEnter: true,
         }}
       >
-        <SwiperSlide>
-          <h1 className="text-white">h1</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h1 className="text-white">h2</h1>
-        </SwiperSlide>
+        {arr.map((item, index) => {
+          return (
+            <SwiperSlide className="flex-center" key={index}>
+              <h1 className="text-white">{item}</h1>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </section>
   );
