3a34fa51105c49cfb536ee6d2ce1c7050353c5c9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 08:45:51 +0330

    Created an array

--- src/Components/TabsSection/TabsSection.jsx
+++ src/Components/TabsSection/TabsSection.jsx
@@ -3,6 +3,7 @@ import SectionHeading from "../Utils/SectionHeading/SectionHeading";
 import { Tab, TabList, Tabs } from "react-tabs";
 
 function TabsSection() {
+  const array = Array.from({ length: 6 }, () => 1);
   return (
     <Tabs className="container flex-center">
       <SectionHeading title="تصاویر ماژول انبارداری" />
