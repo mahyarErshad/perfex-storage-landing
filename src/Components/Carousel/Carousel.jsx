0287264c1467ed601b939e4ac68ef7f69649677f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 13:16:09 +0330

    Initialized the swiper

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import "swiper/swiper-bundle.css";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-function Carousel() {
+const Carousel = () => {
   return (
-    <div>Carousel</div>
-  )
-}
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      loop={true}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+    >
+      <SwiperSlide>
+        <h1 className="text-white">h1</h1>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h1 className="text-white">h2</h1>
+      </SwiperSlide>
+      <h1 className="text-white">h3</h1>
+    </Swiper>
+  );
+};
 
-export default Carousel
+export default Carousel;
