7f242f6cdeea64b5715c95a5a72e8ac390a3aee9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 15:41:03 +0330

    Slides go out of screen by slide animation

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -5,11 +5,11 @@ const Carousel = () => {
   const arr = ["دقت بالا در کنترل موجودی در هنگام نقل و انتقال کالاها در بین انبارها", "دقت بالا در کنترل موجودی در هنگام مرجوع شدن کالاها", "مدیریت راحت انبارها", "سرعت در انجام کارهای مرتبط با انبارداری"];
 
   return (
-    <section className="w-full flex-center px-4">
-      <div className="flex-center gap-6">
+    <section className="w-full flex-center px-4 mb-[6.25rem] carousel-wrapper">
+      <div className="flex-center gap-6 carousels">
         {arr.map((item, index) => {
           return (
-            <div className="flex-center gap-4 glass-container rounded-20 py-4 px-8" key={index}>
+            <div className="flex-center gap-4 glass-container rounded-20 py-4 px-8 carousel" key={index}>
               <DoubleCheck />
               <p className="text-white">{item}</p>
             </div>
