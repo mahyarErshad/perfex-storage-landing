4ee317db4168ab75cf8eb72be9d85d19eacaa8d3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 15:08:44 +0330

    Removed carousel packages

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as DoubleCheck } from "../../assets/images/double-check.
 
 const Carousel = () => {
   const arr = ["دقت بالا در کنترل موجودی در هنگام نقل و انتقال کالاها در بین انبارها", "دقت بالا در کنترل موجودی در هنگام مرجوع شدن کالاها", "مدیریت راحت انبارها", "سرعت در انجام کارهای مرتبط با انبارداری"];
+
   return (
     <section className="w-full flex-center px-4">
       <div className="flex-center gap-6">
