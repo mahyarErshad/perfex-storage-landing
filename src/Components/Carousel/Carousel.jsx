7edef3c83e00c93706bee7c308530492def82094
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 14:33:35 +0330

    Installed slick-carousel react-slick

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -5,14 +5,16 @@ const Carousel = () => {
   const arr = ["دقت بالا در کنترل موجودی در هنگام نقل و انتقال کالاها در بین انبارها", "دقت بالا در کنترل موجودی در هنگام مرجوع شدن کالاها", "مدیریت راحت انبارها", "سرعت در انجام کارهای مرتبط با انبارداری"];
   return (
     <section className="w-full flex-center px-4">
-      {arr.map((item, index) => {
-        return (
-          <div className="flex-center gap-4 glass-container rounded-20 py-4 px-8" key={index}>
-            <DoubleCheck />
-            <p className="text-white">{item}</p>
-          </div>
-        );
-      })}
+      <div className="flex-center gap-6">
+        {arr.map((item, index) => {
+          return (
+            <div className="flex-center gap-4 glass-container rounded-20 py-4 px-8" key={index}>
+              <DoubleCheck />
+              <p className="text-white">{item}</p>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
