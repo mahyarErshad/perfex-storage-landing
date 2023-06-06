3085ecadbe2d0fcc1156e74b4d3144dd26eb5b0a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 08:42:26 +0330

    imported { Tab, TabList, Tabs }

--- src/Components/TabsSection/TabsSection.jsx
+++ src/Components/TabsSection/TabsSection.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import SectionHeading from "../Utils/SectionHeading/SectionHeading";
+import { Tab, TabList, Tabs } from "react-tabs";
 
 function TabsSection() {
   return (
-    <section className="container flex-center">
+    <Tabs className="container flex-center">
       <SectionHeading title="تصاویر ماژول انبارداری" />
-    </section>
+      <TabList className="mt-10 flex-center gap-6">
+        <Tab>عنوان تصویر</Tab>
+      </TabList>
+    </Tabs>
   );
 }
 
