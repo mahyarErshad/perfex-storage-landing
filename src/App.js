6776eba2102d1ab33f618e72e29d61d17c8dae84
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-06 13:20:28 +0330

    Added swiper slides

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroSection from "./Components/HeroSection/HeroSection";
 import AboutSection from "./Components/AboutSection/AboutSection";
 import FeaturesSection from "./Components/FeaturesSection/FeaturesSection";
 import TabsSection from "./Components/TabsSection/TabsSection";
+import Carousel from "./Components/Carousel/Carousel";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AboutSection />
       <FeaturesSection />
       <TabsSection />
+      <Carousel />
     </>
   );
 }
--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import "swiper/swiper-bundle.css";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Carousel = () => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      loop={true}
-      autoplay={{
-        delay: 3000,
-        disableOnInteraction: false,
-      }}
-    >
-      <SwiperSlide>
-        <h1 className="text-white">h1</h1>
-      </SwiperSlide>
-      <SwiperSlide>
-        <h1 className="text-white">h2</h1>
-      </SwiperSlide>
-      <h1 className="text-white">h3</h1>
-    </Swiper>
+    <section className="container flex-center">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide>
+          <h1 className="text-white">h1</h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h1 className="text-white">h2</h1>
+        </SwiperSlide>
+        <h1 className="text-white">h3</h1>
+      </Swiper>
+    </section>
   );
 };
 
