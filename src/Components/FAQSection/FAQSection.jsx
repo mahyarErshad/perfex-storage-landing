7725299631ccbc3edddc3990fa991e1eef4750eb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-07 10:08:43 +0330

    Added arrow icon

--- src/Components/FAQSection/FAQSection.jsx
+++ src/Components/FAQSection/FAQSection.jsx
@@ -3,29 +3,46 @@ import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-
 import SectionHeading from "../Utils/SectionHeading/SectionHeading";
 
 function FAQSection() {
+  const data = [
+    {
+      header: "آیا کالاهای ثبت شده قبلی در اسکریپت در این ماژول نیز قابل استفاده هستند؟",
+    },
+    {
+      header: "آیا می توان چند انبار مختلف را مدیریت کرد؟",
+    },
+    {
+      header: "آیا می شود یک محصول را در چند انبار قرار داد؟",
+    },
+    {
+      header: "آیا می توان یک محصول را در بین انبارهای مختلف جابجا کرد؟",
+    },
+    {
+      header: "آیا امکان انبارگردانی در این ماژول وجود دارد؟",
+    },
+    {
+      header: "آیا امکان تعیین شخص یا ناظر برای کنترل و تایید ورود و خروج کالا از انبارها وجود دارد؟",
+    },
+    {
+      header: "آیا امکان مشاهده وضعیت مرسوله یا بسته ارسالی توسط مشتری وجود دارد؟",
+    },
+  ];
   return (
     <section className="container flex-center mb-[12.1875rem] gap-12">
       <SectionHeading title="سوالات متداول" />
       <Accordion>
-        <AccordionItem>
-          <AccordionHeader>
-            <h3 className={`accordion-title`}>Title 1</h3>
-          </AccordionHeader>
+        {data.map((item, i) => {
+          return (
+            <AccordionItem key={i}>
+              <AccordionHeader className="glass-container justify-between px-6 py-4 rounded-lg w-full">
+                <h3 className="text-white">{item.header}</h3>
+              </AccordionHeader>
 
-          <AccordionBody>
-            <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
-          </AccordionBody>
-        </AccordionItem>
-
-        <AccordionItem>
-          <AccordionHeader>
-            <h3 className={`accordion-title`}>Title 2</h3>
-          </AccordionHeader>
-
-          <AccordionBody>
-            <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
-          </AccordionBody>
-        </AccordionItem>
+              <AccordionBody className="mb-3">
+                <div className="text-white">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد</div>
+              </AccordionBody>
+            </AccordionItem>
+          );
+        })}
       </Accordion>
     </section>
   );
