7938ccefd42ac4ac3cfd2634e1e1bed1dfe23c5e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-07 09:44:38 +0330

    Added Accordion

--- src/Components/FAQSection/FAQSection.jsx
+++ src/Components/FAQSection/FAQSection.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
 
 function FAQSection() {
   return (
-    <div>FAQSection</div>
-  )
+    <section className="container flex-center mb-[12.1875rem]">
+      <Accordion>
+        <AccordionItem>
+          <AccordionHeader>
+            <h3 className={`accordion-title`}>Title 1</h3>
+          </AccordionHeader>
+
+          <AccordionBody>
+            <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
+          </AccordionBody>
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionHeader>
+            <h3 className={`accordion-title`}>Title 2</h3>
+          </AccordionHeader>
+
+          <AccordionBody>
+            <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
+          </AccordionBody>
+        </AccordionItem>
+      </Accordion>
+    </section>
+  );
 }
 
-export default FAQSection
+export default FAQSection;
