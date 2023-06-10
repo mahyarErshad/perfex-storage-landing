2e055c2e07e69cd932520ea402cedf770cb92235
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-10 12:53:23 +0330

    Tabs are created using real data

--- src/Components/TabsSection/TabsSection.jsx
+++ src/Components/TabsSection/TabsSection.jsx
@@ -9,24 +9,51 @@ import image5 from "../../assets/images/Tabs/5.png";
 import image6 from "../../assets/images/Tabs/6.png";
 
 function TabsSection() {
-  const array = Array.from({ length: 6 }, () => 1);
+  const array = [
+    {
+      text: "ایجاد انبارهای مختلف",
+      image: image1,
+    },
+    {
+      text: "مدیریت کالاها و خدمات",
+      image: image2,
+    },
+    {
+      text: "صدور حواله ورودی انبار",
+      image: image3,
+    },
+    {
+      text: "صدور حواله خروجی انبار",
+      image: image4,
+    },
+    {
+      text: "ایجاد حواله مبادلات داخلی",
+      image: image5,
+    },
+    {
+      text: "لیست بارنامه ها",
+      image: image6,
+    },
+  ];
   return (
     <Tabs className="w-full px-4 flex-col mx-auto flex-center mb-32 relative">
       <span className="w-[23.25rem] h-[23.25rem] bg-primary absolute top-[34%] right-[4%] max-lg:hidden blur-[28.125rem]"></span>
       <SectionHeading title="تصاویر ماژول انبارداری" />
       <TabList className="mt-10 flex-center flex-wrap gap-6 mb-20">
-        {array.map((_, index) => (
+        {array.map(({ text }, index) => (
           <Tab className=" flex-center flex-col cursor-pointer duration-300 tab" key={index}>
-            <div className="glass-container rounded-20 py-6 max-h-[4.5rem] text-white w-[12.5rem] font-bold text-center z-10">عنوان تصویر</div>
+            <div className="glass-container rounded-20 py-6 max-h-[4.5rem] text-white w-[12.5rem] font-bold text-center z-10">{text}</div>
             <span className="w-[85%] h-2 -translate-y-1 bg-gradient-to-r from-[#8BC6EC] to-[#9599E2] rounded-20 duration-300 opacity-0 z-0"></span>
           </Tab>
         ))}
       </TabList>
-      {array.map((_, index) => (
-        <TabPanel key={index}>
-          <img src={image1} alt="laptop" />
-        </TabPanel>
-      ))}
+      <div className="w-full min-h-[42rem] flex-center">
+        {array.map(({ text, image }, index) => (
+          <TabPanel key={index}>
+            <img src={image} alt={text} />
+          </TabPanel>
+        ))}
+      </div>
     </Tabs>
   );
 }
