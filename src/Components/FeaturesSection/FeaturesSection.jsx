7356bd4fbff3d0b9c2f3068a65f720f16fa70398
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-10 11:13:17 +0330

    Passed title and description

--- src/Components/FeaturesSection/FeaturesSection.jsx
+++ src/Components/FeaturesSection/FeaturesSection.jsx
@@ -7,7 +7,7 @@ function FeaturesSection() {
     <section className="container flex-center mb-[9.5rem] gap-10">
       <SectionHeading title="ویژگی های ماژول انبارداری" />
       <div className="grid lg:grid-cols-4 md:grid-cols-2 max-md:grid-cols-1 gap-6 w-full">
-        <FeatureSectionCard gradient="from-[#F4D03F] to-[#16A085]" />
+        <FeatureSectionCard title="فرآیندهای بهینه شده کسب و کار" description="ایجاد راه های مناسب برای مدیریت انبار به شما کمک می کند تا فرآیندهای داخل انبار را به آرامی اصلاح کنید.این بدان معناست که نه تنها نیازاست که بهترین فرآیندها را متناسب با خود سازمان داشته باشید،بلکه فرایند هایی که متناسب با نوع محصولات شما باشند را اداره کنید." gradient="from-[#F4D03F] to-[#16A085]" />
         <FeatureSectionCard gradient="from-[#8BC6EC] to-[#9599E2]" />
         <FeatureSectionCard gradient="from-[#FF3C3C] to-[#640000]" />
         <FeatureSectionCard gradient="from-[#FBAB7E] to-[#F7CE68]" />
