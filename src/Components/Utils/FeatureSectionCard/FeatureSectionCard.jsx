9238393cf3a0543b476e976f7288325db3fb7338
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-10 11:22:53 +0330

    Passed title and props to the FeatureSectionCard

--- src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
+++ src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
@@ -5,7 +5,7 @@ function FeatureSectionCard({ gradient, title, description }) {
     <div className="flex-col flex-center">
       <span className={`w-[85%] h-2 rounded-[0.3125rem] bg-gradient-to-r ${gradient}`}></span>
       <div className="flex-col py-10 px-4 -mt-1 glass-container rounded-20 w-full">
-        <h3 className="gradient-text text-center font-bold max-lg:text-xl lg:text-2xl">{title}</h3>
+        <h3 className="gradient-text text-center font-bold max-lg:text-base lg:text-lg">{title}</h3>
         <span className="h-[0.125rem] w-full bg-gradient mt-6"></span>
         <p className="mt-8 w-full text-white text-xs leading-6">{description}</p>
       </div>
