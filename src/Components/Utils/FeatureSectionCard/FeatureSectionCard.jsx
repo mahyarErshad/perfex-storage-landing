da6e644265bcea6e9f4b3e85007bc6a099867d32
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-10 11:34:22 +0330

    Cards have same height

--- src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
+++ src/Components/Utils/FeatureSectionCard/FeatureSectionCard.jsx
@@ -7,7 +7,7 @@ function FeatureSectionCard({ gradient, title, description }) {
       <div className="flex-col py-10 px-4 -mt-1 glass-container rounded-20 w-full">
         <h3 className="gradient-text text-center font-bold max-lg:text-base lg:text-lg">{title}</h3>
         <span className="h-[0.125rem] w-full bg-gradient mt-6"></span>
-        <p className="mt-8 w-full text-white text-xs leading-6">{description}</p>
+        <p className="mt-8 w-full text-white text-xs leading-6 min-h-[9rem]">{description}</p>
       </div>
     </div>
   );
