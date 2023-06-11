c93f881eecfe5c6ccb297f2cb22926132f37921d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-11 11:49:17 +0330

    Using webp image

--- src/Components/TabsSection/TabsSection.jsx
+++ src/Components/TabsSection/TabsSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SectionHeading from "../Utils/SectionHeading/SectionHeading";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import image1 from "../../assets/images/Tabs/1.webp";
-import image2 from "../../assets/images/Tabs/2.png";
+import image2 from "../../assets/images/Tabs/2.webp";
 import image3 from "../../assets/images/Tabs/3.png";
 import image4 from "../../assets/images/Tabs/4.png";
 import image5 from "../../assets/images/Tabs/5.png";
