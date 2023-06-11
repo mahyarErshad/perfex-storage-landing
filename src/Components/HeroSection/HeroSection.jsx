c91da3f172a13f2ac888d909fa5be3f507fe4214
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-11 11:55:46 +0330

    Changed png to webp

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 import CustomButton from "../Utils/CustomButton/CustomButton";
 import image from "../../assets/images/hero-section-image.webp";
 import topRightBoxes from "../../assets/images/hero-boxes-top-right.png";
-import bottomLeftBoxes from "../../assets/images/hero-boxes-bottom-left.png";
+import bottomLeftBoxes from "../../assets/images/hero-boxes-bottom-left.webp";
 import { ReactComponent as Arrow } from "../../assets/images/twisted-arrow.svg";
 
 function HeroSection() {
