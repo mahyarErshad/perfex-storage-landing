1dffb0bb900213ea68a1b4bada4b921c7de20022
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-11 11:51:48 +0330

    Changed png to webp

--- src/Components/TabsSection/TabsSection.jsx
+++ src/Components/TabsSection/TabsSection.jsx
@@ -5,7 +5,7 @@ import image1 from "../../assets/images/Tabs/1.webp";
 import image2 from "../../assets/images/Tabs/2.webp";
 import image3 from "../../assets/images/Tabs/3.webp";
 import image4 from "../../assets/images/Tabs/4.webp";
-import image5 from "../../assets/images/Tabs/5.png";
+import image5 from "../../assets/images/Tabs/5.webp";
 import image6 from "../../assets/images/Tabs/6.png";
 
 function TabsSection() {
