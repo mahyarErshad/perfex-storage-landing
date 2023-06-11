b1611b7f7d35395b5c9a84d3182fe954e80f0967
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-11 14:25:14 +0330

    Passed text and href to the button

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -12,7 +12,7 @@ function HeroSection() {
       <span className="w-[11rem] h-[11rem] bg-primary absolute top-0 left-0 max-lg:hidden -translate-x-[150%] blur-[10.3125rem]"></span>
       <h1 className="font-bold max-lg:text-3xl text-[3.5rem] gradient-text__animation text-center">ماژول انبارداری اسکریپت پرفکس</h1>
       <p className="mt-8 text-center text-base text-white">با ماژول انبارداری اسکریپت مدیریت ارتباط با مشتری Perfex CRM ، فروش خود را افزایش دهید و کسب و کار خود را با راه حل های قدرتمند مدیریت موجودی، انجام سفارشات و کنترل موجودی بهبود بخشید.</p>
-      <CustomButton className="mt-11" />
+      <CustomButton text="پیشنمایش زنده" href="https://perfex-script.com/admin/" className="mt-11" />
       <div className="relative">
         <span className="w-[11rem] h-[11rem] bg-primary absolute top-0 right-0 max-lg:hidden translate-x-[150%] blur-[8.5625rem]"></span>
         <img src={image} alt="محیط کاربری ماژول انبارداری اسکریپت پرفکس" />
